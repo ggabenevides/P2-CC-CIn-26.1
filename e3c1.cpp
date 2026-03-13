8144044e666666c2bd6264f20e40eaa84abe3b6f
ggabenevides <gabtbenevides@gmail.com>
2026-03-12 22:12:32 -0300

    Changes to be committed: 	new file:   e3c1.cpp  Changes not staged for commit: 	deleted:    EDOO/e1c1 	deleted:    EDOO/e1c1.cpp 	deleted:    EDOO/e2c1 	deleted:    EDOO/e2c1.cpp 	deleted:    EDOO/hellocpp 	deleted:    EDOO/hellocpp.cpp 	deleted:    e3c1.cpp  Untracked files: 	EDOO/ap1-1 	EDOO/ap1-1.cpp 	EDOO/ap1-2 	EDOO/ap1-2.cpp 	EDOO/ap1-3 	EDOO/ap1-3.cpp 	EDOO/ap1-4 	EDOO/ap1-4.cpp 	EDOO/ap1-5 	EDOO/ap1-5.cpp

---
+++ e3c1.cpp
@@ -0,0 +1,19 @@
+// A program containing errors!
+#include <iostream> 
+#include <string>
+#include <stdlib.h>
+
+void srand(unsigned int seed);
+
+int main() {
+  std::string message = "\nLearn from your mistakes!";
+  std::cout << message << std::endl;
+  int len = message.length();
+  std::cout << "Length of the string: " << len << std::endl;
+  // And a random number in addition:
+  int a, b;
+  a = srand(12.5);
+  b = rand();
+  std::cout << "\nRandom number: " << b << std::endl;
+ return 0;
+}
