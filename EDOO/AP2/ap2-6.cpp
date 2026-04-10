b0b2177a2efd0ef814af1131acc1f864466f4eba
gtb <gtb@g5c63.cin.ufpe.br>
2026-04-10 09:52:10 -0300

    adicionando ultimo exercicio da lista, ainda incompleto

---
+++ EDOO/AP2/ap2-6.cpp
@@ -0,0 +1,48 @@
+#include <time.h> // Prototype of time()
+#include <stdlib.h> // Prototypes of srand() and rand()
+#include <iostream>
+
+int main(){
+    // initizalizing variables
+    long sec;
+    time(&sec); // Take the number of seconds and
+    srand((unsigned) sec); // use it to initialise.
+    unsigned long long random(20);
+    bool play_again(true);
+    char answer;
+
+    while(play_again)
+
+        // generate a random number that satisfies requirements
+        random = (rand() % 15) + 1;
+
+        // loop for a round of the game - three attempts to guess
+        for (int i = 1; i<=3; i++){
+            int guess;
+            bool guessed(false);
+            std::cout << "Guess a number between 1 and 15:";
+            std::cin >> guess;
+            std::cout << std::endl;
+            std:: cout << "Attempt " << i << " of 3: " << guess << std::endl;
+            if(guess == random){
+                std::cout << "Congratulations! You guessed correctly!";
+                guessed = true;
+                break;
+            }
+            else{
+                std::cout << "Tough luck! You guessed incorrectly.";
+            }
+
+        std::cout << "You-'ve finished this round! Want to play again? (y/n) ";
+        std::cin >> answer;
+        std::cout << std::endl;
+
+        if(answer == 'n'){
+            play_again = false;
+        }
+
+    }
+
+
+}
+
